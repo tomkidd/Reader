72f10b9bab842538ad6129be55d0db7465710734
Tom Kidd <tomkidd@gmail.com>
2017-06-06 16:34:09 -0700

    Grafted in ReaderFramework from mozinator into latest code

---
+++ ReaderFramework/ReaderFramework.h
@@ -0,0 +1,38 @@
+//
+//  ReaderFramework.h
+//  ReaderFramework
+//
+//  Created by mozinator on 10/24/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReaderFramework.
+FOUNDATION_EXPORT double ReaderFrameworkVersionNumber;
+
+//! Project version string for ReaderFramework.
+FOUNDATION_EXPORT const unsigned char ReaderFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReaderFramework/PublicHeader.h>
+
+#import <ReaderFramework/CGPDFDocument.h>
+#import <ReaderFramework/ReaderConstants.h>
+#import <ReaderFramework/ReaderContentPage.h>
+#import <ReaderFramework/ReaderContentTile.h>
+#import <ReaderFramework/ReaderContentView.h>
+#import <ReaderFramework/ReaderDocument.h>
+#import <ReaderFramework/ReaderDocumentOutline.h>
+#import <ReaderFramework/ReaderMainPagebar.h>
+#import <ReaderFramework/ReaderMainToolbar.h>
+#import <ReaderFramework/ReaderThumbCache.h>
+#import <ReaderFramework/ReaderThumbFetch.h>
+#import <ReaderFramework/ReaderThumbQueue.h>
+#import <ReaderFramework/ReaderThumbRender.h>
+#import <ReaderFramework/ReaderThumbRequest.h>
+#import <ReaderFramework/ReaderThumbsView.h>
+#import <ReaderFramework/ReaderThumbView.h>
+#import <ReaderFramework/ReaderViewController.h>
+#import <ReaderFramework/ThumbsMainToolbar.h>
+#import <ReaderFramework/ThumbsViewController.h>
+#import <ReaderFramework/UIXToolbarView.h>
